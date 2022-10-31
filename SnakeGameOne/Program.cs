fcdb9a3c987052439c4870f12242e348528e86c8
JoaquinCubillos1 <juaquito143@hotmail.com>
2022-10-30 23:30:01 -0500

    Primer Cambio al código xd
    Mondongo

--- SnakeGameOne/Program.cs
+++ SnakeGameOne/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SnakeGameOne.ClaseCell;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
@@ -271,54 +272,6 @@ namespace SnakeGameOne
                 toPrint += "\n";
             }
             Console.WriteLine(toPrint);
-        }
-        public class Cell
-        {
-            public string val
-            {
-                get;
-                set;
-            }
-            public int x
-            {
-                get;
-                set;
-            }
-            public int y
-            {
-                get;
-                set;
-            }
-            public bool visited
-            {
-                get;
-                set;
-            }
-            public int decay
-            {
-                get;
-                set;
-            }
-
-            public void decaySnake()
-            {
-                decay -= 1;
-                if (decay == 0)
-                {
-                    visited = false;
-                    val = " ";
-                }
-            }
-
-            public void Clear()
-            {
-                val = " ";
-            }
-
-            public void Set(string newVal)
-            {
-                val = newVal;
-            }
         }
     }
 }
